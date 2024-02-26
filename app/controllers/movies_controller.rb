5086904166c82901056e6835d1d61ad2ea0bdadf
Eduardo Villa <151227243+evilla092@users.noreply.github.com>
2024-02-26 21:53:19 +0000

    finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def new
     @movie = Movie.new
   end
@@ -14,12 +16,9 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    movie_params = params.require(:movie).permit(:title, :description, :image_url, :released_on)
-    
     @movie = Movie.new(movie_params)
 
     if @movie.valid?
@@ -32,14 +31,11 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
     @movie = Movie.find(params.fetch(:id))
 
-    movie_params = params.require(:movie).permit(:title, :description, :image_url, :released_on)
-    
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
     else
@@ -53,5 +49,15 @@ class MoviesController < ApplicationController
     @movie.destroy
 
     redirect_to movies_url, notice: "Movie deleted successfully."
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :image_url)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+
+
 Rails.application.configure do
   # Allow server to be hosted on any URL
   config.hosts.clear
